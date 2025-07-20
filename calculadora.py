5a2e6f80579d9bbb76902a3b1b11938aff78e310
Cristhian Gaitan <cfgaitan63@gmail.com>
2025-07-20 10:47:23 -0500

    create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,21 @@
+# calculadora.py
+
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+def multiplicar(a, b):
+    return a * b
+
+def dividir(a, b):
+    if b == 0:
+        return "Error: división por cero"
+    return a / b
+
+if __name__ == "__main__":
+    print("Suma:", sumar(10, 5))
+    print("Resta:", restar(10, 5))
+    print("Multiplicación:", multiplicar(10, 5))
+    print("División:", dividir(10, 5))
